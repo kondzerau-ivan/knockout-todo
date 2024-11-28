0989402da759d3b13224d59abf28b22fd2f98081
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-11-28 18:51:12 +0300

    add: basic CRUD operations with Knockout.js

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 class ViewModel {
     todos = ko.observableArray([
-        {title: 'learn M2', complited: false},
-        {title: 'learn React', complited: false},
-        {title: 'learn Vue', complited: false},
-        {title: 'learn HTML', complited: true}
+        { id: 1, title: 'learn M2', complited: false },
+        { id: 2, title: 'learn React', complited: false },
+        { id: 3, title: 'learn Vue', complited: false },
+        { id: 4, title: 'learn HTML', complited: true }
     ]);
+
+    addTodo = (element) => {
+        const title = element.title.value;
+        const lastElementIndex = this.todos().length - 1;
+        let id;
+
+        if (lastElementIndex === -1) {
+            id = 1;
+        } else {
+            id = this.todos()[lastElementIndex].id + 1;
+        }
+
+        const newTodo = {
+            id,
+            title,
+            complited: false
+        };
+
+        if (title) {
+            this.todos.push(newTodo);
+            element.reset();
+        }
+    }
+
+    getTodoId = (event) => {
+        return parseInt(event.target.parentElement.id.match(/-(\d+)/)[1]);
+    }
+
+    getTodo = (event) => {
+        return this.todos().find(todo => todo.id === this.getTodoId(event));
+    }
+
+    removeTodo = (element, event) => {
+        const id = this.getTodoId(event);
+        const todo = this.getTodo(event);
+
+        if (id && todo) {
+            this.todos.remove(todo);
+        }
+    }
+
+    compliteTodo = (element, event) => {
+        const id = this.getTodoId(event);
+        const todo = this.getTodo(event);
+        const updatedTodo = { ...todo, complited: true };
+
+        if (id && todo) {
+            this.todos.replace(todo, updatedTodo);
+        }
+    }
 }
 
 ko.applyBindings(new ViewModel);
